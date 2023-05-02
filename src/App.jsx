da609bc68d82fd0fb3b5bfc435dc15abad6af08d
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-05-02 13:13:28 -0300

    build: deploy settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Catalog from "./pages/Catalog";
 import Auth from "./pages/Auth";
 import FilmCollection from "./pages/FilmCollection";
